0e34afcc6657b5dd1ec4d496bb15cca4dbbc5702
Hybridost <romansin06@gmail.com>
2025-12-22 00:07:20 +0300

    Add files via upload

---
+++ lababa.py
@@ -0,0 +1,13 @@
+"""Модуль для операций с числами"""
+
+def add(a, b):
+    """Сложение двух чисел"""
+    return a + b
+
+def multiply(a, b):
+    """Умножение двух чисел"""
+    return a * b
+
+def power(base, exponent):
+    """Возведение в степень"""
+    return base ** exponent
